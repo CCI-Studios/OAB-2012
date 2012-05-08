3db38d48531acf86b80773566ff047086f311ced
Matt Elliott <melliott@ccistudios.com>
2012-05-08 14:04:47 -0400

    compiled css

---
+++ config/deploy/staging.rb
@@ -0,0 +1,10 @@
+# repository info
+set :branch, "development"
+
+# This may be the same as your `Web` server
+role :app, "ccistudios.com"
+
+# directories
+set :deploy_to, "/home/arbors/subdomains/dev"
+set :public, "#{deploy_to}/public_html"
+set :extensions, %w[plg_ie6 public template]
